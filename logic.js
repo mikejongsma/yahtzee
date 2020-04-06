e4daff303acdbce822fffae77141d36af2a62171
Mike Jongsma <mikejongsma@live.nl>
2020-04-06 14:49:24 +0200

    Rewrite multiple functions to a single function - rmDie

--- logic.js
+++ logic.js
@@ -11,43 +11,55 @@ console.log('Welkom bij het dobbelspel Yahtzee. klik op de knop om de dobbelsten
 
 // Functie voor het 'gooien' van de dobbelstenen.
 function throwDice() {
+    // tel het aantal elementen in de diceInitArray
+    console.log("Aantal dobbelstenen in de dobbelbak: " + diceInitArray.length);
+    // wanneer lengte van de dobbelbak array gelijk is aan 5 vraag dan toestemming om alle dobbelstenen opnieuw te gooien.
+    if(diceInitArray.length == 5){
+        console.log("Er liggen 5 dobbelstenen in de dobbelbak, je hebt geen stenen aangewezen om opnieuw te gooien. Daarom gooi ik alle dobbelstenen opnieuw!");
+        diceInitArray = [];
+        return;
+    }
+    if(diceInitArray.length < 5){
+        console.log("Aantal dobbelstenen in de dobbelbak is lager dan 5.");
+        console.log("Het aantal stenen om te gooien: " + diceNr);
+    }
+    
     for (i = 0; i < diceNr; i++) {
         let diceNr = Math.floor(Math.random() * 6) + 1;
         let cc = document.getElementById('diceBox');
         diceInitArray.push(diceNr);
         console.log(diceNr);
-        // console.log(diceInitArray);
 
         // switch voor de verschillende opties
         switch (diceNr) {
             case 1:
                 let box1 = document.createElement('div');
-                box1.innerHTML = "<div id=\"d1\" class=\"diceOne\" onclick=\"rm1()\"><div class=\"dot\"></div></div>";
+                box1.innerHTML = "<div id=\"d1\" class=\"diceOne\" onclick=\"rmDie(id)\"><div class=\"dot\"></div></div>";
                 cc.appendChild(box1);
                 break;
             case 2:
                 let box2 = document.createElement('div');
-                box2.innerHTML = "<div id=\"d2\" class=\"diceTwo\" onclick=\"rm2()\"><div class=\"dot\"></div><div class=\"dot\"></div></div>";
+                box2.innerHTML = "<div id=\"d2\" class=\"diceTwo\" onclick=\"rmDie(id)\"><div class=\"dot\"></div><div class=\"dot\"></div></div>";
                 cc.appendChild(box2);
                 break;
             case 3:
                 let box3 = document.createElement('div');
-                box3.innerHTML = "<div id=\"d3\" class=\"diceThree\" onclick=\"rm3()\"><div class=\"dot\"></div><div class=\"dot\"></div><div class=\"dot\"></div></div>";
+                box3.innerHTML = "<div id=\"d3\" class=\"diceThree\" onclick=\"rmDie(id)\"><div class=\"dot\"></div><div class=\"dot\"></div><div class=\"dot\"></div></div>";
                 cc.appendChild(box3);
                 break;
             case 4:
                 let box4 = document.createElement('div');
-                box4.innerHTML = "<div id=\"d4\" class=\"diceFour\" onclick=\"rm4()\"><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div></div>";
+                box4.innerHTML = "<div id=\"d4\" class=\"diceFour\" onclick=\"rmDie(id)\"><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div></div>";
                 cc.appendChild(box4);
                 break;
             case 5:
                 let box5 = document.createElement('div');
-                box5.innerHTML = "<div id=\"d5\" class=\"diceFive\" onclick=\"rm5()\"><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div><div class=\"dot\"></div><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div></div>";
+                box5.innerHTML = "<div id=\"d5\" class=\"diceFive\" onclick=\"rmDie(id)\"><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div><div class=\"dot\"></div><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div></div></div>";
                 cc.appendChild(box5);
                 break;
             case 6:
                 let box6 = document.createElement('div');
-                box6.innerHTML = "<div id=\"d6\" class=\"diceSix\" onclick=\"rm6()\"><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div><div class=\"dot\"></div></div><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div><div class=\"dot\"></div></div></div>";
+                box6.innerHTML = "<div id=\"d6\" class=\"diceSix\" onclick=\"rmDie(id)\"><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div><div class=\"dot\"></div></div><div class=\"column\"><div class=\"dot\"></div><div class=\"dot\"></div><div class=\"dot\"></div></div></div>";
                 cc.appendChild(box6);
                 break;
         }
@@ -61,55 +73,16 @@ function throwDice() {
 }
 
 // Behoud van dobbelstenen (Hier 1 functie voor schrijven)
-function rm1() {
-    console.log('Deze dobbelsteen wil ik opnieuw gooien');
-    let reThrow = document.getElementById('d1');
+
+function rmDie(id){
+    console.log(id);
+    console.log('Deze dobbelsteen wil ik opnieuw gooien: ' + id);
+    let reThrow = document.getElementById(id);
     reThrow.remove();
-    removeArray.push(1);
+    removeArray.push(id);
+    diceNr == diceNr - 1;
     console.log(removeArray);
 }
-
-function rm2() {
-    console.log('Deze dobbelsteen wil ik opnieuw gooien');
-    let reThrow = document.getElementById('d2');
-    reThrow.remove();
-    removeArray.push(2);
-    console.log(removeArray);
-}
-
-function rm3() {
-    console.log('Deze dobbelsteen wil ik opnieuw gooien');
-    let reThrow = document.getElementById('d3');
-    reThrow.remove();
-    removeArray.push(3);
-    console.log(removeArray);
-}
-
-function rm4() {
-    console.log('Deze dobbelsteen wil ik opnieuw gooien');
-    let reThrow = document.getElementById('d4');
-    reThrow.remove();
-    removeArray.push(4);
-    console.log(removeArray);
-}
-
-function rm5() {
-    console.log('Deze dobbelsteen wil ik opnieuw gooien');
-    let reThrow = document.getElementById('d5');
-    reThrow.remove();
-    removeArray.push(5);
-    console.log(removeArray);
-}
-
-function rm6() {
-    console.log('Deze dobbelsteen wil ik opnieuw gooien');
-    let reThrow = document.getElementById('d6');
-    reThrow.remove();
-    removeArray.push(6);
-    console.log(removeArray);
-}
-
-
 
 // Laat score of spelregels zien
 
